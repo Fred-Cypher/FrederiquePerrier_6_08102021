e42641d5fa241c12d08d64dae7ffab78c7c960b7
Fred Perrier <frederique.perrier@gmail.com>
2021-10-28 14:44:17 +0200

    Petites correction et changements de lignes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 const path = require('path');
-
 
 const app = express();
 app.use(express.json());
