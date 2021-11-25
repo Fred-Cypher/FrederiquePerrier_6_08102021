79cf19f16125ccee81f819726730ad7661968194
Fred Perrier <frederique.perrier@gmail.com>
2021-11-25 09:45:31 +0100

    Correction authentification pour le Delete

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -52,11 +52,17 @@ exports.modifySauce = (req, res, next) => {
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
+            if (!sauce){
+                res.status(404).json({ error :new Error('Pas de sauce trouvée')});
+            }
+            if(sauce.userId !== req.auth.userId){
+                res.status(400).json({ error: new Error('Requête non autorisée')});
+            }
             const filename = sauce.imageUrl.split('/images/')[1];
             fs.unlink(`images/${filename}`, () => {  // Permet de supprimer le fichier du dossier "images"
-                Sauce.deleteOne({ _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Sauce supprimée' }))
-                    .catch(error => res.status(400).json({ error }));
+            Sauce.deleteOne({ _id: req.params.id })
+                .then(() => res.status(200).json({ message: 'Sauce supprimée' }))
+                .catch(error => res.status(400).json({ error }));
             });
         })
         .catch(error => res.status(500).json({ error }));
